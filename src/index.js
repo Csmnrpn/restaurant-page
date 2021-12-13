0f540ea5389a12aac50eae74a43daf532fecca14
Cosmin R <cosminripan33@gmail.com>
2021-12-13 22:22:36 +0200

    added and styled navbar

--- src/index.js
+++ src/index.js
@@ -7,6 +7,33 @@ import {
 } from './assets/js/contact';
 import './assets/css/style.css';
 
-// frontFunction();
+
+const body = document.querySelector('body');
+
+const navbar = document.createElement('div');
+navbar.classList.add('navbar');
+body.appendChild(navbar);
+
+const navigation = document.createElement('ul');
+navigation.classList.add('navigation');
+navbar.appendChild(navigation);
+
+const navigationItems = ['home', 'menu', 'contact'];
+
+navigationItems.forEach(item => {
+    const navItem = document.createElement('li');
+    const navText = document.createElement('h2');
+    navText.innerText = item.toUpperCase();
+    navItem.appendChild(navText);
+    navigation.appendChild(navItem);
+
+    navItem.addEventListener('click', (item) => {
+
+    })
+})
+
+
+
+frontFunction();
 // createMenu();
-createContactPage();
+// createContactPage();
